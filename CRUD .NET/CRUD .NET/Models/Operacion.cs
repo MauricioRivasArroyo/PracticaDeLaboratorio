4645dd30e961768356330dbd8234e2d7a5a14596
mauriziocx <you@example.com>
2019-05-16 01:34:48 -0500

    .NET added

---
+++ CRUD .NET/CRUD .NET/Models/Operacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.NET.Models
+{
+    public class Operacion
+    {
+        public int id { get; set; }
+
+        public string operacion { get; set; }
+
+        public float primero { get; set; }
+
+
+    }
+}
